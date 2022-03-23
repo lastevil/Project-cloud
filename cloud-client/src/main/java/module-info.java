f1cb4b1033a16dad57e457a0f999a6830fb708a2
lastik <lastevilday@gmail.com>
2022-03-23 11:02:24 +0500

    edit maven pom

--- cloud-client/src/main/java/module-info.java
+++ cloud-client/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module gb.project.cloud.client.cloudclient {
     requires javafx.fxml;
     requires cloud.objects;
     requires io.netty.codec;
+    requires org.slf4j;
 
     opens gb.project.cloud.client to javafx.fxml;
     exports gb.project.cloud.client;
